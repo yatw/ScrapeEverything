64738c30b64bc7ad0f3319a146877bef328dbfb0
yatw <yatw@uci.edu>
2019-10-14 12:29:13 -0700

    Created ASP.NET API Project

---
+++ ScrapeEverything/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ScrapeEverything.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ScrapeEverything/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ScrapeEverything.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
